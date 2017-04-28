ffc0de48e6d21423f65dfeb34043b9e12384d61a
Daniel Rampelt <drampelt@gmail.com>
2017-04-27 23:42:37 -0400

    Show movies in RecyclerView, add internet permission

--- app/src/main/java/org/equithon/equithondemo/MainActivity.java
+++ app/src/main/java/org/equithon/equithondemo/MainActivity.java
@@ -2,6 +2,8 @@ package org.equithon.equithondemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.Toast;
 
 import java.util.List;
@@ -14,10 +16,15 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView recyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://gist.githubusercontent.com")
@@ -30,6 +37,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onResponse(Call<List<Movie>> call, Response<List<Movie>> response) {
                 List<Movie> movies = response.body();
+                MovieAdapter movieAdapter = new MovieAdapter(movies);
+                recyclerView.setAdapter(movieAdapter);
             }
 
             @Override
