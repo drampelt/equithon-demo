930c458057e9d95a39c191240c59045e4854ce57
Daniel Rampelt <drampelt@gmail.com>
2017-04-27 23:27:48 -0400

    Add MovieAdapter

---
+++ app/src/main/java/org/equithon/equithondemo/MovieAdapter.java
@@ -0,0 +1,60 @@
+package org.equithon.equithondemo;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieHolder> {
+    private List<Movie> movies;
+
+    public MovieAdapter(List<Movie> movies) {
+        this.movies = movies;
+    }
+
+    @Override
+    public MovieHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_movie, parent, false);
+        return new MovieHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(MovieHolder holder, int position) {
+        Movie movie = movies.get(position);
+        holder.bind(movie);
+    }
+
+    @Override
+    public int getItemCount() {
+        return movies.size();
+    }
+
+
+    class MovieHolder extends RecyclerView.ViewHolder {
+
+        private ImageView moviePoster;
+        private TextView movieTitle;
+        private TextView moviePlot;
+
+        public MovieHolder(View itemView) {
+            super(itemView);
+            moviePoster = (ImageView) itemView.findViewById(R.id.moviePoster);
+            movieTitle = (TextView) itemView.findViewById(R.id.movieTitle);
+            moviePlot = (TextView) itemView.findViewById(R.id.moviePlot);
+        }
+
+        public void bind(Movie movie) {
+            movieTitle.setText(movie.title + " (" + movie.year + ")");
+            moviePlot.setText(movie.simplePlot);
+            Picasso.with(itemView.getContext())
+                    .load(movie.urlPosterPreview)
+                    .into(moviePoster);
+        }
+    }
+}
